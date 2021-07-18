effff049c4f8c1b775487029fba3769aa881f5a4
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-18 03:36:37 +0300

    Pushed all

---
+++ SymbolStreams/ISymbolStream.cs
@@ -0,0 +1,27 @@
+﻿namespace SymbolStreams
+{
+    /// <summary>
+    /// Interface for symbols streams
+    /// </summary>
+    /// <typeparam name="T">A type that represents characters in a stream</typeparam>
+    public interface ISymbolStream<T>
+    {
+        /// <summary>
+        /// Returns current symbol at the stream
+        /// </summary>
+        /// <returns>Current stream symbol</returns>
+        public T PeekSymbol();
+
+        /// <summary>
+        /// Returns next symbol at the stream
+        /// </summary>
+        /// <returns>Next stream symbol</returns>
+        public T NextSymbol();
+
+        /// <summary>
+        /// Checks if the stream has reached the end of the file
+        /// </summary>
+        /// <returns>Returns true if stream reaches the end of file, otherwise returns false</returns>
+        public bool EndOfStream();
+    }
+}
---
+++ SymbolStreams/LoadedStream.cs
@@ -0,0 +1,37 @@
+﻿namespace SymbolStreams
+{
+    /// <summary>
+    /// Unoptimized stream that loads all information from input at once
+    /// </summary>
+    /// <typeparam name="T">A type that represents characters in a stream</typeparam>
+    public class LoadedStream<T> : ISymbolStream<T>
+    {
+        /// <summary>
+        /// Information from input
+        /// </summary>
+        private T[] _loadedStream;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private int _position = 0;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public virtual T PeekSymbol() => _loadedStream[_position];
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public virtual T NextSymbol() => _loadedStream[++_position];
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool EndOfStream() => _position == _loadedStream.Length;
+    }
+}

7e7a5b9e2a59b2bb76a2f0f4de4a970df64f5751
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-19 03:03:16 +0300

    Updated tests

---
+++ SymbolStreams.Tests/IterativeLoadedStreamTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace SymbolStreams.Tests
+{
+    public class IterativeLoadedStreamTests
+    {
+        [Fact]
+        public void IteratorCorrectReadDataTest()
+        {
+            // Arrange 
+            string testData = "Hello World";
+            IterativeLoadedStream<char> iterativeLoadedStream = new(testData.ToCharArray());
+
+            // Act
+            string result = "";
+            foreach (char ch in iterativeLoadedStream) result += ch;
+
+            // Assert
+            Assert.Equal(testData, result);
+        }
+
+    }
+}
